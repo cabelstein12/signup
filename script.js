3fa3e4e351b25e68b37ec12d01fbcdbf578b089d
cabelstein12 <Caleb.Steinwandt@gmail.com>
2023-11-09 04:57:29 -0700

    <style>[optional_scope]: <add dynamic elements to webpage>
    [optional_body]
    
    [optional_footer]

---
+++ script.js
@@ -0,0 +1,17 @@
+// const firstName = document.getElementById('first_name');
+// // firstName.onkeyup;
+// const lastName = document.getElementById('last_name');
+// const email = document.getElementById('email');
+// const password = document.getElementById('password');
+// const passwordConfirm = document.getElementById('password_confirmation');
+
+// function validator(input) {
+//     console.log('you are here')
+//     if(input.value.length > 3){
+//         console.log('validated!')
+//         input.style.borderColor = 'green';
+//     } else if (input.value.length > 0 && input.value.length < 3){
+//         input.style.borderColor = 'red';
+//     }
+// }
+//  validator(firstName)
