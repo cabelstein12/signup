6f1bcc0a7599527125ba965c2bde23b9d688b941
cabelstein12 <Caleb.Steinwandt@gmail.com>
2023-11-10 11:14:43 -0700

    <style>[optional_scope]: <complete error handling update input/ label value relationship adjust animation timing>
    [optional_body]
    
    [optional_footer]

--- script.js
+++ script.js
@@ -1,17 +1,46 @@
-// const firstName = document.getElementById('first_name');
-// // firstName.onkeyup;
-// const lastName = document.getElementById('last_name');
-// const email = document.getElementById('email');
-// const password = document.getElementById('password');
-// const passwordConfirm = document.getElementById('password_confirmation');
+function matchPassword(){
+    let password = document.getElementById('password').value;
+    let passwordConfirmation = document.getElementById('password_confirmation').value;
+    if (passwordConfirmation !== password) {
+        // Create a div element for the error message
+        let errorMessage = document.createElement('div');
+        errorMessage.textContent = 'Passwords do not match!';
+        errorMessage.style.color = 'red';
 
-// function validator(input) {
-//     console.log('you are here')
-//     if(input.value.length > 3){
-//         console.log('validated!')
-//         input.style.borderColor = 'green';
-//     } else if (input.value.length > 0 && input.value.length < 3){
-//         input.style.borderColor = 'red';
-//     }
-// }
-//  validator(firstName)
+        // Insert the error message after the password confirmation input
+        let passwordConfirmationInput = document.getElementById('password_confirmation');
+        passwordConfirmationInput.parentNode.insertBefore(errorMessage, passwordConfirmationInput.nextSibling);
+    }
+}
+
+let inputNames = document.querySelectorAll('input[type="text"]');
+let namePattern = /^[A-Za-z]+$/;
+inputNames.forEach((input) => {
+    input.setAttribute('pattern', namePattern.source);
+})
+
+let inputs = Array.from(document.querySelectorAll('input'));
+let labels = Array.from(document.querySelectorAll('label'))
+
+inputs.forEach((input) => {
+    input.addEventListener('keyup', () => {
+            let label = labels[inputs.indexOf(input)];
+        if(input.value.length > 0){
+            // console.log(inputs.indexOf(input))
+            label.style.fontSize = '0.75em';
+            label.style.top = '-18%';
+            label.style.transition = '0.2s linear'
+        } else {
+            label.style.fontSize = '1em';
+            label.style.top = 'initial';
+        }
+        if(input.checkValidity()){
+            input.style.borderColor ='initial';
+        }
+        else{
+            input.style.borderColor = 'red';
+        }
+
+    })
+})
+
